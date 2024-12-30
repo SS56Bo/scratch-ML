1b6d44e422e51a81a333cf68017bc536d6c01129
SS56Bo <sohamsarkar560@gmail.com>
2024-12-30 20:47:09 +0530

    Loss Function implementation

--- linearRegression.py
+++ linearRegression.py
@@ -6,8 +6,8 @@ Y = np.array([100, 115, 111, 150, 142, 147, 121])   #for loss function
 Y_train = np.dot(Y, 1.0) # for conversion into float
 
 #training set
-print(f"X Trained Data: {X_train}")
-print(f"Y Trained Data: {Y_train}\n")
+print(f"X Training Data: {X_train}")
+print(f"Y Training Data: {Y_train}\n")
 
 #Training set shape
 print(f"Shape of training set: {X_train.shape}")
@@ -15,18 +15,12 @@ print(f"Number of training set examples: {X_train.shape[0]}\n")
 
 #dataset
 for i in range(X_train.shape[0]):
-    print (f"[X^{i}, Y^{i}] = [{X_train[i]}, {Y_train[i]}]")
+    print (f"[X^({i}), Y^({i})] = [{X_train[i]}, {Y_train[i]}]")
 
 #weights for linear regression
 w = 100
 b = 100
 
-#data visualization using Matplotlib
-plot.scatter(X_train, Y_train, c='g', marker="X")
-plot.title("Linear Regression")  #for the title
-plot.ylabel("Y-label")
-plot.xlabel("X-label")
-plot.show()
 
 # equation for optimization
 def linearRegression(X_train, w, b):
@@ -38,6 +32,28 @@ def linearRegression(X_train, w, b):
     return f_wb
 
 print(linearRegression(X_train, w, b))
+
 # loss function 
+def CostFunction(X_train, w, b):
+    N=X_train.shape[0]
+    y_rec = np.zeros(N)
+    costSum=0
+    for i in range(N):
+        y_rec[i]=np.dot(w,X_train[i])+b
+        cost = (1/N)*((Y_train[i]-y_rec[i])**2)
+        costSum+=cost
+
+    return costSum
+
+print(f"Cost Function: {CostFunction(X_train,w,b)}")
 
 # gradient descent 
+
+#data visualization using Matplotlib
+y_recv = linearRegression(X_train, w, b)
+plot.plot(X_train, y_recv, c='b')
+plot.scatter(X_train, Y_train, c='g', marker="X")
+plot.title("Linear Regression")  #for the title
+plot.ylabel("Y-label")
+plot.xlabel("X-label")
+plot.show()
