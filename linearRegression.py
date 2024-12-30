b3114999b63c33cdd419f386119e56e7a8b9faf9
SS56Bo <sohamsarkar560@gmail.com>
2024-12-30 20:22:12 +0530

    Data Visualization for Linear Regression

--- linearRegression.py
+++ linearRegression.py
@@ -1,5 +1,5 @@
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plot
 
 X_train = np.array([2.5, 3.6, 4.5, 7.8, 5.6, 6.2, 5.6 ])  # our primary component
 Y = np.array([100, 115, 111, 150, 142, 147, 121])   #for loss function
@@ -7,11 +7,11 @@ Y_train = np.dot(Y, 1.0) # for conversion into float
 
 #training set
 print(f"X Trained Data: {X_train}")
-print(f"Y Trained Data: {Y_train}")
+print(f"Y Trained Data: {Y_train}\n")
 
 #Training set shape
 print(f"Shape of training set: {X_train.shape}")
-print(f"Number of training set examples: {X_train.shape[0]}")
+print(f"Number of training set examples: {X_train.shape[0]}\n")
 
 #dataset
 for i in range(X_train.shape[0]):
@@ -21,6 +21,12 @@ for i in range(X_train.shape[0]):
 w = 0
 b = 0
 
+#data visualization using Matplotlib
+plot.scatter(X_train, Y_train, c='g', marker="2")
+plot.title("X vs. Y")  #for the title
+plot.ylabel("Y-label")
+plot.xlabel("X-label")
+plot.show()
 
 # equation for optimization
 
