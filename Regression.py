8dae4b39078eef187acb0b105fa6c2ab4b0969e5
SS56Bo <sohamsarkar560@gmail.com>
2025-01-02 01:44:28 +0530

    Regression: Cost Function Added

--- Regression.py
+++ Regression.py
@@ -3,7 +3,7 @@ import numpy as np
 class LinearRegression:
     
     def __init__(self, w, b):
-        self.w = np.array(w, dtype=float)
+        self.w = float(w)
         self.b = float(b)
 
     def Regression(self, X):
@@ -14,3 +14,16 @@ class LinearRegression:
             f_wb[i]=np.dot(self.w, X[i])+self.b
         
         return f_wb
+
+    def CostFunctionCalc(self, X, test):
+        X=np.array(X, dtype=float)
+        test=np.array(test, dtype=float)
+        costSum = 0
+        N = X.shape[0]
+        f_wb = np.zeros(N)
+        for i in range(N):
+            f_wb[i]=np.dot(self.w, X[i])+self.b
+            cost = (1/N)*((test[i]-f_wb[i])**2)
+            costSum += cost
+        
+        return costSum
