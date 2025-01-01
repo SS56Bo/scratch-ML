bebaae6f60b65eeb5bf1cfa30ebd46c3dd44f2f0
SS56Bo <sohamsarkar560@gmail.com>
2025-01-02 01:33:03 +0530

    Linear Regression: Class implementation

--- Regression.py
+++ Regression.py
@@ -1,3 +1,16 @@
+import numpy as np
+
 class LinearRegression:
-    def equationPlug(w,b):
-        return w,b
+    
+    def __init__(self, w, b):
+        self.w = np.array(w, dtype=float)
+        self.b = float(b)
+
+    def Regression(self, X):
+        X= np.array(X, dtype=float)
+        N = X.shape[0]
+        f_wb = np.zeros(N)
+        for i in range(N):
+            f_wb[i]=np.dot(self.w, X[i])+self.b
+        
+        return f_wb
