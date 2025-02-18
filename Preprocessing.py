9f483722311ea01b01c35a380addddd0e03788a8
SS56Bo <sohamsarkar560@gmail.com>
2025-02-18 15:49:09 +0530

    Tests cleared

--- Preprocessing.py
+++ Preprocessing.py
@@ -4,3 +4,14 @@ class StandardScaler():
     def __init__(self):
         self.mean = None
         self.std = None
+    
+    def calcFit(self, X):
+        self.mean = np.mean(X, axis=0)
+        self.std = np.std(X, axis=0)
+    
+    def Ztransform(self, X):
+        return (X-self.mean)/self.std
+    
+    def fit_transform(self, X):
+        self.calcFit(X)
+        return self.Ztransform(X)
